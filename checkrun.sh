e0ec7413f1dab9e34283a38074488fc37c35e03c
Karlatemp <karlatemp@vip.qq.com>
2022-04-08 23:23:26 +0800

    test checkrun

--- checkrun.sh
+++ checkrun.sh
@@ -5,4 +5,4 @@ curl \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $GH_TOKEN" \
   https://api.github.com/repos/KasukuSakura/test/check-runs \
-  -d '{"name":"TEST","head_sha":"6401e1e0b66a0738f8d6e0832f178759babdf8a8","status":"completed","conclusion": "success","output":{"text":"Hi","summary":"summary","text":"text"}}'
+  -d '{"name":"TEST","head_sha":"6401e1e0b66a0738f8d6e0832f178759babdf8a8","status":"completed","conclusion": "success","output":{"title":"Hi","summary":"summary","text":"text"}}'
