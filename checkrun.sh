948c817d3c68d1c3d43dbc6830923f38a101e306
Karlatemp <karlatemp@vip.qq.com>
2022-04-08 23:02:56 +0800

    test checkrun

---
+++ checkrun.sh
@@ -0,0 +1,8 @@
+
+# shellcheck disable=SC2016
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GH_TOKEN" \
+  https://api.github.com/repos/octocat/hello-world/check-runs \
+  -d '{"name":"TEST","head_sha":"6401e1e0b66a0738f8d6e0832f178759babdf8a8","status":"completed","conclusion": "success","output":{"text":"Hi","summary":"summary","text":"text"}}'
